9c687c88b38a0f9801994ca0e0d8fa255a4d0397
Lucas Gabriel Ruiz <lucaschatoruiz@gmail.com>
2025-06-11 18:20:43 -0300

    init project

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Cube Cat</h1>
+      <div className="card">
+        <p>
+          Sitio en construcción
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
